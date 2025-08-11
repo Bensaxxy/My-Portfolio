6c9d262cc932d8a6739ccc61fe08e9547814f6e4
Oluwasegun Benjamin Tobi <benjaminsegun38@gmail.com>
2025-08-11 13:48:04 +0100

    Implement code changes to enhance functionality and improve performance

--- src/Components/Pages/Portfolio/graphDesign.js
+++ src/Components/Pages/Portfolio/graphDesign.js
@@ -3,21 +3,26 @@
 const data = [
   {
     id: 1,
+    title: "Embrace Night",
+    imageUrl: "./Embrace Night.jpg",
+  },
+  {
+    id: 2,
     title: "Wedds IV",
     imageUrl: "./WEDDS IV-01.jpg",
   },
   {
-    id: 2,
+    id: 3,
     title: "FastMore Logistics",
     imageUrl: "./fastmore.jpg",
   },
   {
-    id: 3,
+    id: 4,
     title: "Anny Luxery Homes",
     imageUrl: "./annyluxery.jpg",
   },
   {
-    id: 4,
+    id: 5,
     title: "Ayooluwa Garment Store",
     imageUrl: "./AYOOLUWA GARMENT STORE.jpg",
   },
